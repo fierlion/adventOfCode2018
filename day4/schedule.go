46ae8bdacff59d6b90d0dd248e54d30678983f42
Ray Allan <fierlion@amazon.com>
2019-03-11 20:22:55 +0000

    finished day4

--- day4/schedule.go
+++ day4/schedule.go
@@ -10,13 +10,15 @@ import (
 )
 
 type guardEntry struct {
-	month       int
-	day         int
-	hour        int
-	minute      int
-	guardId     int
-	asleepAwake bool
+	month   int
+	day     int
+	hour    int
+	minute  int
+	guardId int
+	isAwake bool
 }
+
+const SHIFT_SIZE = 24 * 60
 
 func main() {
 	inputFile, _ := filepath.Abs("./testInputSorted")
@@ -27,6 +29,7 @@ func main() {
 	defer file.Close()
 
 	var entries []*guardEntry
+	var sleepResults = make(map[int][SHIFT_SIZE]int)
 
 	scanner := bufio.NewScanner(file)
 	currentGuard := 0
@@ -41,6 +44,35 @@ func main() {
 		entries = append(entries, entry)
 	}
 
+	// set sleep bits for each guard.
+	// assumes guards will only have shifts between 00:00 and 23:59
+	// -1 means guard starts shift
+	currentGuardFallsAsleep := -1
+	for _, entry := range entries {
+		currentTime := (entry.hour * 60) + entry.minute
+		if !entry.isAwake {
+			currentGuardFallsAsleep = currentTime
+		} else if currentGuardFallsAsleep >= 0 {
+			if sleepResult, ok := sleepResults[entry.guardId]; ok {
+				for i := currentGuardFallsAsleep; i < currentTime; i++ {
+					sleepResult[i] += 1
+				}
+				sleepResults[entry.guardId] = sleepResult
+			} else {
+				var thisResult [SHIFT_SIZE]int
+				for i := currentGuardFallsAsleep; i < currentTime; i++ {
+					thisResult[i] += 1
+				}
+				sleepResults[entry.guardId] = thisResult
+			}
+			currentGuardFallsAsleep = -1
+		}
+	}
+	for id, result := range sleepResults {
+		thisResult, thisIndex := findMaxInt(result)
+		fmt.Printf("guard %d: %d, %d\n", id, thisResult, thisIndex)
+		fmt.Printf("array %v\n", result)
+	}
 }
 
 // entryString is format: "[1518-03-05 23:57] Guard #2963 begins shift"
@@ -48,12 +80,12 @@ func parseEntry(entryString string) *guardEntry {
 	splitString := strings.Split(entryString, " ")
 	rawDate, rawTime, rawConscious, rawId := splitString[0][1:], splitString[1], splitString[2], splitString[3]
 	thisId := 0
-	thisAsleepAwake := true
+	thisIsAwake := true
 	if rawConscious == "Guard" {
 		newId, _ := strconv.Atoi(rawId[1:])
 		thisId = newId
 	} else {
-		thisAsleepAwake = (rawConscious == "wakes")
+		thisIsAwake = (rawConscious == "wakes")
 	}
 
 	// get date and time
@@ -65,11 +97,23 @@ func parseEntry(entryString string) *guardEntry {
 	thisMinute, _ := strconv.Atoi(splitTime[1][:2])
 
 	return &guardEntry{
-		month:       thisMonth,
-		day:         thisDay,
-		hour:        thisHour,
-		minute:      thisMinute,
-		guardId:     thisId,
-		asleepAwake: thisAsleepAwake,
+		month:   thisMonth,
+		day:     thisDay,
+		hour:    thisHour,
+		minute:  thisMinute,
+		guardId: thisId,
+		isAwake: thisIsAwake,
 	}
+}
+
+func findMaxInt(sleepResult [SHIFT_SIZE]int) (int, int) {
+	maxValue := 0
+	maxIndex := -1
+	for idx, value := range sleepResult {
+		if value >= maxValue {
+			maxValue = value
+			maxIndex = idx
+		}
+	}
+	return maxValue, maxIndex
 }
